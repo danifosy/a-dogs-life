468a7bd16bde46ee657d051cd4cad7574655967f
danifosy <mail@daniha.de>
2021-09-07 14:45:26 +0200

    Fix header types from p to h1-3

--- src/app/components/Typography/Typography.tsx
+++ src/app/components/Typography/Typography.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import type { ReactNode } from 'react';
-import styles from './Typhography.module.css';
+import styles from './Typography.module.css';
 
 type TyphographyProps = {
   size: 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
   children: ReactNode;
 };
 
-export default function Typhography({
+export default function Typography({
   size,
   children,
 }: TyphographyProps): JSX.Element {
@@ -18,10 +18,10 @@ export default function Typhography({
   } else if (size === 'm') {
     return <p className={styles.textMedium}>{children}</p>;
   } else if (size === 'l') {
-    return <p className={styles.textLarge}>{children}</p>;
+    return <h3 className={styles.textLarge}>{children}</h3>;
   } else if (size === 'xl') {
-    return <p className={styles.textExtraLarge}>{children}</p>;
+    return <h2 className={styles.textExtraLarge}>{children}</h2>;
   } else {
-    return <p className={styles.textExtraExtraLarge}>{children}</p>;
+    return <h1 className={styles.textExtraExtraLarge}>{children}</h1>;
   }
 }
