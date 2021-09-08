3d4b0e490e134e9d84635d67af5a387eb5ef97f7
danifosy <mail@daniha.de>
2021-09-08 18:31:10 +0200

    Add icons into props

--- src/app/components/SubmitButton/SubmitButton.tsx
+++ src/app/components/SubmitButton/SubmitButton.tsx
@@ -3,11 +3,26 @@ import type { ReactNode } from 'react';
 import styles from './SubmitButton.module.css';
 
 type SubmitButtonProps = {
+  icon:
+    | 'ArrowRight'
+    | 'Camera'
+    | 'Letter'
+    | 'Safe'
+    | 'Search'
+    | 'Send'
+    | 'Signup'
+    | 'Upload';
   children: ReactNode;
 };
 
 export default function submitButton({
+  icon,
   children,
 }: SubmitButtonProps): JSX.Element {
-  return <button className={styles.submitButton}>{children}</button>;
+  return (
+    <button className={styles.submitButton}>
+      <img src={`src/assets/SVGs/${icon}.svg`} className={styles.icon} />
+      {children}
+    </button>
+  );
 }
