efde1a48a306fb93e1a8009ddf5164af49dd29a9
danifosy <mail@daniha.de>
2021-09-08 18:54:16 +0200

    Import and add Typography to SubmitButton

--- src/app/components/SubmitButton/SubmitButton.tsx
+++ src/app/components/SubmitButton/SubmitButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { ReactNode } from 'react';
 import styles from './SubmitButton.module.css';
+import Typography from '../Typography/Typography';
 
 type SubmitButtonProps = {
   icon:
@@ -15,14 +16,14 @@ type SubmitButtonProps = {
   children: ReactNode;
 };
 
-export default function submitButton({
+export default function SubmitButton({
   icon,
   children,
 }: SubmitButtonProps): JSX.Element {
   return (
     <button className={styles.submitButton}>
       <img src={`src/assets/SVGs/${icon}.svg`} className={styles.icon} />
-      {children}
+      <Typography size="s">{children}</Typography>
     </button>
   );
 }
