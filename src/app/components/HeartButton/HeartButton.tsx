981ecfc3d3cb2144a4dd21c6a66fb94fa4adef98
danifosy <mail@daniha.de>
2021-09-09 16:03:50 +0200

    Write Heartbutton.tsx

---
+++ src/app/components/HeartButton/HeartButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import HeartIcon from '../../../assets/HeartIcon';
+import styles from './HeartButton.module.css';
+
+export type HeartButtonProps = {
+  onButtonClick: () => void;
+  isActive: boolean;
+};
+
+export default function HeartButton({
+  isActive,
+  onButtonClick,
+}: HeartButtonProps): JSX.Element {
+  const active = {
+    fill: 'var(--action-color)',
+    stroke: 'var(--action-color)',
+  };
+
+  const inactive = {
+    fill: 'var(--background-color)',
+    stroke: 'var(--action-color)',
+  };
+
+  if (isActive === true) {
+    active;
+  } else {
+    inactive;
+  }
+  return (
+    <button onClick={onButtonClick} className={styles.heartButton}>
+      <HeartIcon />
+    </button>
+  );
+}
