838ed872f848442c1aa9ce10f557961382056ea5
danifosy <mail@daniha.de>
2021-09-10 09:22:18 +0200

    Delete if statement and rename active/inactive vars

--- src/app/components/HeartButton/HeartButton.tsx
+++ src/app/components/HeartButton/HeartButton.tsx
@@ -11,24 +11,19 @@ export default function HeartButton({
   isActive,
   onButtonClick,
 }: HeartButtonProps): JSX.Element {
-  const active = {
+  const activeStyles = {
     fill: 'var(--action-color)',
     stroke: 'var(--action-color)',
   };
 
-  const inactive = {
+  const inactiveStyles = {
     fill: 'var(--background-color)',
     stroke: 'var(--action-color)',
   };
 
-  if (isActive === true) {
-    active;
-  } else {
-    inactive;
-  }
   return (
     <button onClick={onButtonClick} className={styles.heartButton}>
-      <HeartIcon {...(isActive ? active : inactive)} />
+      <HeartIcon {...(isActive ? activeStyles : inactiveStyles)} />
     </button>
   );
 }
