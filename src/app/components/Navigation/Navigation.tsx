5ce1eb79bfacfbecf333155a6eafe335198077fa
danifosy <mail@daniha.de>
2021-09-10 14:36:33 +0200

    Export prop type

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -3,15 +3,15 @@ import type { ReactNode } from 'react';
 import styles from './Navigation.module.css';
 import Typography from '../Typography/Typography';
 
-type NavigationProps = {
+export type NavigationProps = {
   children: ReactNode;
 };
 
 export default function Navigation({ children }: NavigationProps): JSX.Element {
   return (
     <>
-      <ul className={styles.BurgerButton_List}>
-        <li className={styles.BurgerButton_ListItem}>
+      <ul className={styles.Navigation_List}>
+        <li className={styles.Navigation_ListItem}>
           <Typography size="l">
             <a href="" className={styles.Navigation_ListItem_Text}>
               {children}
