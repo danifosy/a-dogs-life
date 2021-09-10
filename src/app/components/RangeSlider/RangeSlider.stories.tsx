fa54eb1d283ff617b558bfceea388a6eb5e80416
danifosy <mail@daniha.de>
2021-09-10 11:18:01 +0200

    Write RangeSlider story

---
+++ src/app/components/RangeSlider/RangeSlider.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import RangeSlider from './RangeSlider';
+
+export default {
+  title: 'Component/RangeSlider',
+  component: RangeSlider,
+};
+
+export const WeightSelection = (): JSX.Element => <RangeSlider />;
