ea508abc73530231c832ae22113b31b85c5737d0
danifosy <mail@daniha.de>
2021-09-13 14:51:38 +0200

    Import BackIcon.tsx instead of svg in BackButton.tsx

--- src/app/components/BackButton/BackButton.tsx
+++ src/app/components/BackButton/BackButton.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import BackIcon from '../../../assets/BackIcon';
+import styles from './Backbutton.module.css';
 
 export default function BackButton(): JSX.Element {
   return (
-    <button>
-      <img src="src/assets/SVGs/BackButton.svg" />
+    <button className={styles.icon}>
+      <BackIcon />
     </button>
   );
 }
