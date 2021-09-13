311fb3754e870e2b8470cbdc9039c8109a94ca62
danifosy <mail@daniha.de>
2021-09-13 14:39:35 +0200

    Write component and story

---
+++ src/app/components/BackButton/BackButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BackButton(): JSX.Element {
+  return (
+    <button>
+      <img src="src/assets/SVGs/BackButton.svg" />
+    </button>
+  );
+}
