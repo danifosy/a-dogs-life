fa1974c0e950fdf2dd67c544a7f00790670362c1
danifosy <mail@daniha.de>
2021-09-13 11:21:55 +0200

    Change naming of classes

---
+++ src/app/components/BurgerButtonMenu/ BurgerButtonMenu.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './ BurgerButtonMenu.module.css';
+import Navigation from '../Navigation/Navigation';
+import type { NavigationProps } from '../Navigation/Navigation';
+
+export default function BurgerButtonMenu({
+  children,
+}: NavigationProps): JSX.Element {
+  return (
+    <nav>
+      <div className={styles.burgerButton_container}>
+        <div className={styles.burgerButton_overlay}>
+          <Navigation>{children}</Navigation>
+        </div>
+      </div>
+    </nav>
+  );
+}
