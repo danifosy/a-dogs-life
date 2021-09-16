99e55e4c27e7f0a0ada8371b6abd3c15cc7d2d49
danifosy <mail@daniha.de>
2021-09-16 16:13:25 +0200

    Add ID tags to label and input

--- src/app/components/TextInput/TextInput.tsx
+++ src/app/components/TextInput/TextInput.tsx
@@ -4,18 +4,20 @@ import styles from './TextInput.module.css';
 import Typography from '../Typography/Typography';
 
 type TextInputProps = {
+  id: string;
   inputType: 'text' | 'password';
   children: ReactNode;
 };
 
 export default function TextInput({
+  id,
   inputType,
   children,
 }: TextInputProps): JSX.Element {
   return (
     <div className={styles.inputField}>
-      <input type={inputType} required className={styles.input} />
-      <label className={styles.label}>
+      <input id={id} type={inputType} required className={styles.input} />
+      <label htmlFor={id} className={styles.label}>
         <span className={styles.labelContent}>
           <Typography size="s">{children}</Typography>
         </span>
