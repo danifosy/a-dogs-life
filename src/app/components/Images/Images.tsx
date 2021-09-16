ae107f11ba2020504f94164f639b739763195747
danifosy <mail@daniha.de>
2021-09-16 11:49:35 +0200

    Change src attribute input in Images

--- src/app/components/Images/Images.tsx
+++ src/app/components/Images/Images.tsx
@@ -5,7 +5,5 @@ export type ImagesProps = {
   className: string | undefined;
 };
 export default function Images({ image, className }: ImagesProps): JSX.Element {
-  return (
-    <img src={`src/assets/MockImages/${image}.png`} className={className} />
-  );
+  return <img src={image} className={className} />;
 }
