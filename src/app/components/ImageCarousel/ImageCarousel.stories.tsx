ac0d20f60a43a4d2a9ca7a04cb4dd31bdf889222
danifosy <mail@daniha.de>
2021-09-17 15:44:42 +0200

    Add more mock images to story

---
+++ src/app/components/ImageCarousel/ImageCarousel.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import ImageCarousel from './ImageCarousel';
+import MockImage1 from '../../assets/MockImages/Mino1.png';
+import MockImage2 from '../../assets/MockImages/Mino2.png';
+import MockImage3 from '../../assets/MockImages/Mino4.png';
+
+export default {
+  title: 'Component/Carousel',
+  component: Carousel,
+};
+
+const MockImageGallery = [MockImage1, MockImage2, MockImage3];
+
+export const DogSlideCarousel = (): JSX.Element => (
+  <ImageCarousel showArrows={true} image={MockImageGallery} />
+);
