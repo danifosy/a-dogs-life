2daaff17991d7ae0dfdef9ef98258389df49c7aa
danifosy <mail@daniha.de>
2021-09-20 11:51:34 +0200

    Add conditional rendering

--- src/app/pages/BurgerMenu/BurgerMenu.tsx
+++ src/app/pages/BurgerMenu/BurgerMenu.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BurgerButton from '../../components/BurgerButton/BurgerButton';
 import BurgerButtonMenu from '../../components/BurgerButtonMenu/BurgerButtonMenu';
 import styles from './BurgerMenu.module.css';
 
 export default function BurgerMenu(): JSX.Element {
+  const [opened, setOpened] = useState(false);
+
+  function handleOpened() {
+    setOpened(!opened);
+  }
+
   return (
-    <div className={styles.burgerMenu}>
+    <>
       <div className={styles.burgerButton}>
-        <BurgerButton isActive={true} />
+        <BurgerButton isActive={opened} onButtonClick={handleOpened} />
       </div>
-      <BurgerButtonMenu>Home</BurgerButtonMenu>
-      <BurgerButtonMenu>Suche</BurgerButtonMenu>
-      <BurgerButtonMenu>Favoriten</BurgerButtonMenu>
-      <BurgerButtonMenu>Einstellungen</BurgerButtonMenu>
-    </div>
+      {opened === true && (
+        <div className={styles.burgerMenu}>
+          <BurgerButtonMenu>Home</BurgerButtonMenu>
+          <BurgerButtonMenu>Suche</BurgerButtonMenu>
+          <BurgerButtonMenu>Favoriten</BurgerButtonMenu>
+          <BurgerButtonMenu>Einstellungen</BurgerButtonMenu>
+        </div>
+      )}
+    </>
   );
 }
