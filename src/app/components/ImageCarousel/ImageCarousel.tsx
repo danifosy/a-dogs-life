78a88c75cf5f32da7e5fecc82fc5083dd89dc645
danifosy <mail@daniha.de>
2021-09-20 17:51:58 +0200

    Add dynamic height prop

--- src/app/components/ImageCarousel/ImageCarousel.tsx
+++ src/app/components/ImageCarousel/ImageCarousel.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import Image from '../Image/Image';
-
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 type imageCarouselProps = {
   showArrows: boolean;
+  dynamicHeight: boolean;
   image: string[];
 };
 
 export default function ImageCarousel({
   showArrows,
   image,
+  dynamicHeight,
 }: imageCarouselProps): JSX.Element {
   return (
-    <Carousel showArrows={showArrows}>
+    <Carousel showArrows={showArrows} dynamicHeight={dynamicHeight}>
       {image.map((url) => (
         <Image imageURL={url} />
       ))}
