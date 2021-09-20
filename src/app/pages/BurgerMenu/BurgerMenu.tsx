04964e4ba121c08712c32780c395e00bd73cb436
danifosy <mail@daniha.de>
2021-09-20 10:26:21 +0200

    Add components to page

--- src/app/pages/BurgerMenu/BurgerMenu.tsx
+++ src/app/pages/BurgerMenu/BurgerMenu.tsx
@@ -2,3 +2,15 @@ import React from 'react';
 import BurgerButton from '../../components/BurgerButton/BurgerButton';
 import BurgerButtonMenu from '../../components/BurgerButtonMenu/BurgerButtonMenu';
 import styles from './BurgerMenu.module.css';
+
+export default function BurgerMenu(): JSX.Element {
+  return (
+    <div className={styles.BurgerMenu}>
+      <BurgerButton isActive={true} />
+      <BurgerButtonMenu>Home</BurgerButtonMenu>
+      <BurgerButtonMenu>Suche</BurgerButtonMenu>
+      <BurgerButtonMenu>Favoriten</BurgerButtonMenu>
+      <BurgerButtonMenu>Einstellungen</BurgerButtonMenu>
+    </div>
+  );
+}
