db3048610667597ba149e180852d1ec432ab50c1
danifosy <mail@daniha.de>
2021-09-20 10:18:55 +0200

    Create pages folder

--- src/app/components/SlideShow/SlideShow.tsx
+++ src/app/components/SlideShow/SlideShow.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './SlideShow.module.css';
-import Images from '../Image/Image';
+import Image from '../Image/Image';
 import type { ImagesProps } from '../Image/Image';
 
 export default function SlideShow({ imageURL }: ImagesProps): JSX.Element {
   return (
     <>
       <div className={styles.Slider}>
-        <Images imageURL={`${imageURL}`} />
+        <Image imageURL={`${imageURL}`} />
       </div>
     </>
   );
---
+++ src/app/pages/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import BurgerButton from '../../components/BurgerButton/BurgerButton';
+import BurgerButtonMenu from '../../components/BurgerButtonMenu/BurgerButtonMenu';
+import styles from './BurgerMenu.module.css';
