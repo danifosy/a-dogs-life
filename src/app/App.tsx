793c4dcfb70f6067cac26207329590aa7e22ed82
danifosy <mail@daniha.de>
2021-09-21 15:14:34 +0200

    Add route to App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import BurgerMenu from './pages/BurgerMenu/BurgerMenu';
 import DetailDogCard from './pages/DetailDogCard/DetailDogCard';
+import ContactForm from './pages/ContactForm/ContactForm';
 
 function App(): JSX.Element {
   return (
     <>
       <BurgerMenu />
       <BrowserRouter>
+        <Switch>
+          <Route path="/contact">
+            <ContactForm />
+          </Route>
+        </Switch>
         <Switch>
           <Route path="/detailcard">
             <DetailDogCard />
