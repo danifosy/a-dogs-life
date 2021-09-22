ca76c2aed44abee9f2be7a565420d13b0f5f2d83
danifosy <mail@daniha.de>
2021-09-22 10:48:57 +0200

    Add input fields

--- src/app/pages/SettingsForm/SettingsForm.tsx
+++ src/app/pages/SettingsForm/SettingsForm.tsx
@@ -2,11 +2,32 @@ import React from 'react';
 import TextInput from '../../components/TextInput/TextInput';
 import SubmitButton from '../../components/SubmitButton/SubmitButton';
 import styles from './SettingsForm.module.css';
+import Typography from '../../components/Typography/Typography';
 
 export default function SettingsForm(): JSX.Element {
   return (
     <main>
-      <form></form>
+      <form className={styles.settings}>
+        <h2>
+          <Typography size="l">Einstellungen</Typography>
+        </h2>
+        <TextInput inputType="text" id="new-name">
+          Neuer Name
+        </TextInput>
+        <TextInput inputType="text" id="new-mail">
+          Neue Email
+        </TextInput>
+        <TextInput inputType="password" id="new-password1">
+          Neues Passwort
+        </TextInput>
+        <TextInput inputType="password" id="new-password2">
+          Neues Passwort bestätigen
+        </TextInput>
+        <TextInput inputType="password" id="old-password">
+          Altes Passwort
+        </TextInput>
+        <SubmitButton icon="safe">Speichern</SubmitButton>
+      </form>
     </main>
   );
 }
