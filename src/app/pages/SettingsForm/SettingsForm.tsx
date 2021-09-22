ff5a68fbea042f96354c0fcf767b114b21673d6c
danifosy <mail@daniha.de>
2021-09-22 10:35:25 +0200

    Write basic settings form page

---
+++ src/app/pages/SettingsForm/SettingsForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextInput from '../../components/TextInput/TextInput';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
+import styles from './SettingsForm.module.css';
+
+export default function SettingsForm(): JSX.Element {
+  return (
+    <main>
+      <form></form>
+    </main>
+  );
+}
