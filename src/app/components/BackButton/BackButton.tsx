b1a0c1d6f1a8029d4185fed00691077c986ef65a
danifosy <mail@daniha.de>
2021-09-23 14:05:27 +0200

    Fix backbutton function

--- src/app/components/BackButton/BackButton.tsx
+++ src/app/components/BackButton/BackButton.tsx
@@ -3,11 +3,8 @@ import BackIcon from '../../assets/BackIcon';
 import styles from './BackButton.module.css';
 
 export default function BackButton(): JSX.Element {
-  function goBack() {
-    history.back();
-  }
   return (
-    <button className={styles.icon} onClick={goBack}>
+    <button className={styles.icon} onClick={() => history.back()}>
       <BackIcon />
     </button>
   );
