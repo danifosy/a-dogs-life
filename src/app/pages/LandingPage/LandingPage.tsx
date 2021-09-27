ecb824868094c682581c04156645ffebd4eb79f1
danifosy <mail@daniha.de>
2021-09-27 14:08:32 +0200

    Write and style Landing Page

---
+++ src/app/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typography from '../../components/Typography/Typography';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
+import { Link } from 'react-router-dom';
+import styles from './LandingPage.module.css';
+
+export default function LandingPage(): JSX.Element {
+  return (
+    <main className={styles.landingPage}>
+      <Typography size="xxl">A Dog's Life</Typography>
+      <Typography size="s">
+        Finde deinen neuen besten Freund und startet gemeinsam in ein
+        großartiges Abenteuer! Wir helfen dir, den perfekten Hund für deine
+        Ansprüche zu finden, damit ihr zusammen viele schöne Stunden verbringen
+        könnt.
+      </Typography>
+      <Link to="/signupselection" className={styles.button}>
+        <SubmitButton icon="signup">Anmelden</SubmitButton>
+      </Link>
+      <Link to="/login" className={styles.loginText}>
+        <Typography size="s">
+          Hast du bereits einen Account? <br />
+          Hier geht's zum Login.
+        </Typography>
+      </Link>
+      <img src="src/app/assets/SVGs/dog.svg" className={styles.dogSVG} />
+    </main>
+  );
+}
