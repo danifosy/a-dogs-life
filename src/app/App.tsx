cfe58ea36879b17d077a867d6bb733954a3bfa2a
danifosy <mail@daniha.de>
2021-09-29 15:24:28 +0200

    Write login process in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,6 @@ import ContactForm from './pages/ContactForm/ContactForm';
 import SettingsForm from './pages/SettingsForm/SettingsForm';
 import SearchForm from './pages/SearchForm/SearchForm';
 import BackButton from './components/BackButton/BackButton';
-import SignUpSelection from './pages/SignUpSelection/SignUpSelection';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import MainPage from './pages/MainPage/MainPage';
@@ -15,10 +14,84 @@ import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
 import MainPageARA from './pages/MainPageARA/MainPageARA';
 import UploadPage from './pages/UploadPage/UploadPage';
 
+export type UserType = 'ARA' | 'user';
+
+export type StateType = {
+  isAuthenticated: boolean;
+  userName?: string;
+  userType?: UserType;
+};
+
+export type ActionType = {
+  type: string;
+  payload: any;
+};
+
+export type AuthContextType = {
+  state: StateType;
+  dispatch: (event: ActionType) => void;
+};
+
+const initialState: StateType = {
+  isAuthenticated: false,
+  userName: undefined,
+  userType: undefined,
+};
+
+const loadInitialState = () => {
+  const userNameJson = localStorage.getItem('userName');
+  const userTypeJson = localStorage.getItem('userType');
+
+  if (!userNameJson || !userTypeJson) {
+    return initialState;
+  }
+
+  const userName = JSON.parse(userNameJson);
+  const userType = JSON.parse(userTypeJson) as UserType;
+
+  return {
+    isAuthenticated: true,
+    userName,
+    userType,
+  };
+};
+
+export const AuthContext = React.createContext<AuthContextType>({
+  state: initialState,
+  dispatch: () => null,
+});
+
+const reducer = (state: StateType, action: ActionType) => {
+  switch (action.type) {
+    case 'LOGIN':
+      localStorage.setItem('userName', JSON.stringify(action.payload.userName));
+      localStorage.setItem('userType', JSON.stringify(action.payload.userType));
+      return {
+        ...state,
+        isAuthenticated: true,
+        userName: action.payload.userName,
+        userType: action.payload.userType,
+      };
+    case 'LOGOUT':
+      localStorage.removeItem('userName');
+      localStorage.removeItem('userType');
+      return {
+        ...state,
+        isAuthenticated: false,
+        userName: undefined,
+        userType: undefined,
+      };
+    default:
+      return state;
+  }
+};
+
 function App(): JSX.Element {
+  const [state, dispatch] = React.useReducer(reducer, loadInitialState());
+
   return (
-    <>
-      <BurgerMenu />
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {state.isAuthenticated && <BurgerMenu />}
       <BackButton />
 
       <BrowserRouter>
@@ -51,10 +124,6 @@ function App(): JSX.Element {
             <SignUpPage />
           </Route>
 
-          <Route path="/signupselection">
-            <SignUpSelection />
-          </Route>
-
           <Route path="/favorites">
             <FavoritesPage />
           </Route>
@@ -72,7 +141,7 @@ function App(): JSX.Element {
           </Route>
         </Switch>
       </BrowserRouter>
-    </>
+    </AuthContext.Provider>
   );
 }
 
